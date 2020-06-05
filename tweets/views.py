d17e4468a2b122a8e428393ac5347ed91aa1d1c4
Reborn Dzre <reborndzre@gmail.com>
2020-06-05 11:00:55 -0400

    Initial Commit

--- tweets/views.py
+++ tweets/views.py
@@ -7,22 +7,28 @@ from .models import Tweet
 # Create your views here.
 
 def home(request, *args, **kwargs):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'pages/home.html', context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Android
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweet_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "response": tweet_list
+    }
+    return JsonResponse(data)
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
-
-    :param request:
-    :param tweet_id:
-    :param args:
-    :param kwargs:
-    :return:
-
     REST API VIEW
     Consume by JavaScript or Swift/Java/iOS/Android
     return json data
-
     """
     data = {
         "id": tweet_id,
